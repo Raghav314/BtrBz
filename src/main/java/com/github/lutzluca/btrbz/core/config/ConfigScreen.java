4348780ceb2d82269dcdee0811d30b608c0c0670
lutzluca0 <lutzluca0@gmail.com>
2025-11-20 02:19:40 +0100

    feat: introduces subgroup handling in option groups

--- src/main/java/com/github/lutzluca/btrbz/core/config/ConfigScreen.java
+++ src/main/java/com/github/lutzluca/btrbz/core/config/ConfigScreen.java
@@ -3,12 +3,19 @@ package com.github.lutzluca.btrbz.core.config;
 import com.github.lutzluca.btrbz.BtrBz;
 import dev.isxander.yacl3.api.ConfigCategory;
 import dev.isxander.yacl3.api.Option;
+import dev.isxander.yacl3.api.OptionEventListener.Event;
 import dev.isxander.yacl3.api.YetAnotherConfigLib;
 import dev.isxander.yacl3.api.YetAnotherConfigLib.Builder;
 import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ConfigScreen {
 
@@ -55,5 +62,97 @@ public class ConfigScreen {
 
     public static BooleanControllerBuilder createBooleanController(Option<Boolean> option) {
         return BooleanControllerBuilder.create(option).onOffFormatter().coloured(true);
+    }
+
+    public static final class OptionGrouping {
+
+        private final @NotNull Option.Builder<Boolean> controllerBuilder;
+        private final @NotNull List<GroupChild> children;
+        private @Nullable Option<Boolean> controllerOption = null;
+
+        public OptionGrouping(@NotNull Option.Builder<Boolean> controllerBuilder) {
+            this.controllerBuilder = controllerBuilder;
+            this.children = new ArrayList<>();
+        }
+
+        public OptionGrouping addOptions(Option.Builder<?>... optBuilders) {
+            Arrays
+                .stream(optBuilders)
+                .map(Option.Builder::build)
+                .map(GroupChild.SingleOption::new)
+                .forEach(children::add);
+            return this;
+        }
+
+        public OptionGrouping addSubgroups(OptionGrouping... subgroups) {
+            Arrays.stream(subgroups).map(GroupChild.Subgroup::new).forEach(children::add);
+            return this;
+        }
+
+        public List<Option<?>> build() {
+            assert this.controllerOption != null : "OptionGrouping already built";
+
+            var opts = this.children
+                .stream()
+                .flatMap(child -> child.build().stream())
+                .collect(Collectors.toList());
+
+            this.controllerBuilder.addListener((option, event) -> {
+                if (event == Event.STATE_CHANGE || event == Event.AVAILABILITY_CHANGE) {
+                    this.propagateAvailability();
+                }
+            });
+
+            this.controllerOption = this.controllerBuilder.build();
+            this.propagateAvailability();
+
+            opts.addFirst(this.controllerOption);
+            return opts;
+        }
+
+        void setAvailable(boolean available) {
+            assert this.controllerOption != null : "Must call `build` before `setAvailable`";
+            this.controllerOption.setAvailable(available);
+        }
+
+        private void propagateAvailability() {
+            if (this.controllerOption == null) { return; }
+
+            boolean childAvailable = this.controllerOption.available() && this.controllerOption.pendingValue();
+            this.children.forEach(child -> child.setAvailable(childAvailable));
+        }
+
+        private sealed interface GroupChild {
+
+            List<Option<?>> build();
+
+            void setAvailable(boolean available);
+
+            record SingleOption(Option<?> opt) implements GroupChild {
+
+                @Override
+                public List<Option<?>> build() {
+                    return List.of(this.opt);
+                }
+
+                @Override
+                public void setAvailable(boolean available) {
+                    this.opt.setAvailable(available);
+                }
+            }
+
+            record Subgroup(OptionGrouping subgroup) implements GroupChild {
+
+                @Override
+                public List<Option<?>> build() {
+                    return this.subgroup.build();
+                }
+
+                @Override
+                public void setAvailable(boolean available) {
+                    this.subgroup.setAvailable(available);
+                }
+            }
+        }
     }
 }
